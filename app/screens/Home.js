5b9b2e713879fafb581e24a29e826c7a11c2d34c
AmadorZcv <lucasamadorzcv@gmail.com>
2019-09-21 09:49:59 -0300

    Removendo consoles

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,9 +1,8 @@
-import React, { PureComponent } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import { Icon } from 'react-native-elements';
-import { watchPosition } from '../config/geolocation';
-import api from '../config/api';
-
+import React, { PureComponent } from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { Icon } from "react-native-elements";
+import { watchPosition } from "../config/geolocation";
+import api from "../config/api";
 
 export default class Home extends PureComponent {
   constructor(props) {
@@ -16,111 +15,106 @@ export default class Home extends PureComponent {
       isOnline: false,
       zootec: false,
       sentido: false
-
     };
   }
   geo_success = () => {
     //console.log();
-  }
+  };
   isAtivoSet = () => {
     const isAtivo = !this.state.isAtivo;
 
-    this.setState({ isAtivo })
-  }
+    this.setState({ isAtivo });
+  };
 
   zootecSet = () => {
     const zootec = !this.state.zootec;
-    this.setState({ zootec })
-  }
-  updateBage = (position) => {
-    console.log("aqui")
+    this.setState({ zootec });
+  };
+  updateBage = position => {
     const { isAtivo, zootec, sentido } = this.state;
     const { latitude, longitude } = position.coords;
-    api.put('/api/bages', 
-    { bage: { latitude, longitude, one_signal: "wwewe", rodando: isAtivo, sentido: sentido, zootec: zootec } });
-
-  }
+    api.put("/api/bages", {
+      bage: {
+        latitude,
+        longitude,
+        one_signal: "wwewe",
+        rodando: isAtivo,
+        sentido: sentido,
+        zootec: zootec
+      }
+    });
+  };
   watchPosition = async () => {
-    await watchPosition(this.updateBage, (error) => (console.log(error)))
-
-  }
+    await watchPosition(this.updateBage, error => console.log(error));
+  };
 
   componentDidMount() {
-    console.log("asdasdasdasdsadasdasdsad")
     this.watchPosition();
-
   }
-  renderIconSentido = (sentido) => (
-    sentido ? <Icon
-      name='long-arrow-left'
-      type='font-awesome'
-      size={42}
-    /> : <Icon
-        name='long-arrow-right'
-        type='font-awesome'
-        size={42}
-      />
-
-  )
+  renderIconSentido = sentido =>
+    sentido ? (
+      <Icon name="long-arrow-left" type="font-awesome" size={42} />
+    ) : (
+      <Icon name="long-arrow-right" type="font-awesome" size={42} />
+    );
 
   render() {
     const { isAtivo, sentido, zootec } = this.state;
     return (
-      <View style={{ flex: 1, padding: 10, alignItems: 'center', }}>
+      <View style={{ flex: 1, padding: 10, alignItems: "center" }}>
         <TouchableOpacity
           style={{
             margin: 10,
             backgroundColor: isAtivo ? "green" : "red",
-            justifyContent: 'center', height: 50,
-            width: 200, alignItems: 'center',
+            justifyContent: "center",
+            height: 50,
+            width: 200,
+            alignItems: "center"
           }}
           onPress={this.isAtivoSet}
         >
-          <Text >Rodando</Text>
+          <Text>Rodando</Text>
         </TouchableOpacity>
 
-        <View style={{
-          flexDirection: 'row',
-          width: "100%",
-          justifyContent: 'space-between',
-          alignItems: 'center',
-        }}
+        <View
+          style={{
+            flexDirection: "row",
+            width: "100%",
+            justifyContent: "space-between",
+            alignItems: "center"
+          }}
         >
           <Icon
-            name='home-city'
-            type='material-community'
+            name="home-city"
+            type="material-community"
             size={32}
             color="gray"
-
           />
-          <TouchableOpacity style={{
-            marginVertical: 10,
-            width: 200,
-          }}
-            onPress={() => (this.setState({ sentido: !sentido }))}>
+          <TouchableOpacity
+            style={{
+              marginVertical: 10,
+              width: 200
+            }}
+            onPress={() => this.setState({ sentido: !sentido })}
+          >
             {this.renderIconSentido(sentido)}
           </TouchableOpacity>
-          <Icon
-            name='gate'
-            type='material-community'
-            size={32}
-            color="gray"
-          />
-
+          <Icon name="gate" type="material-community" size={32} color="gray" />
         </View>
         <TouchableOpacity
           style={{
             margin: 10,
             backgroundColor: zootec ? "green" : "red",
-            justifyContent: 'center', height: 50,
-            width: 200, alignItems: 'center',
+            justifyContent: "center",
+            height: 50,
+            width: 200,
+            alignItems: "center"
           }}
           onPress={this.zootecSet}
         >
-          <Text >Zootecnia</Text>
+          <Text>Zootecnia</Text>
         </TouchableOpacity>
       </View>
     );
   }
 }
-
