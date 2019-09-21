6d620340a5177ddb7fa0529c7ff89bb62f6a6820
AmadorZcv <lucasamadorzcv@gmail.com>
2019-09-21 10:42:20 -0300

    Inicio novo layout

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
-import { Icon } from "react-native-elements";
+import { View, TouchableOpacity, StyleSheet } from "react-native";
+import SwitchToggle from "react-native-switch-toggle";
+import { Icon, Text, Button } from "react-native-elements";
 import { watchPosition, clearWatches } from "../config/geolocation";
 import api from "../config/api";
 
@@ -58,63 +59,118 @@ export default class Home extends PureComponent {
     ) : (
       <Icon name="long-arrow-right" type="font-awesome" size={42} />
     );
-
+  renderTextSentido() {
+    const { sentido } = this.state;
+    return sentido ? "Indo para o Prédio" : "Indo para o Portão";
+  }
+  renderTextZootec() {
+    const { zootec } = this.state;
+    return zootec ? "Vai para Zootecnia" : " Não Vai para Zootecnia";
+  }
   render() {
     const { rodando, sentido, zootec } = this.state;
     return (
-      <View style={{ flex: 1, padding: 10, alignItems: "center" }}>
-        <TouchableOpacity
-          style={{
-            margin: 10,
-            backgroundColor: rodando ? "green" : "red",
-            justifyContent: "center",
-            height: 50,
-            width: 200,
-            alignItems: "center"
-          }}
-          onPress={this.switchRodando}
+      <View
+        style={{
+          flex: 1,
+          padding: 10,
+          alignItems: "center",
+          justifyContent: "center"
+        }}
+      >
+        <View
+          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
         >
-          <Text>Rodando</Text>
-        </TouchableOpacity>
-
+          <Text style={{ color: rodando ? "green" : "red" }} h2>
+            Rodando
+          </Text>
+          <SwitchToggle
+            switchOn={rodando}
+            onPress={this.switchRodando}
+            backgroundColorOn={"green"}
+            circleColorOn={"white"}
+            circleColorOff={"red"}
+            containerStyle={{
+              marginTop: 16,
+              width: 300,
+              borderRadius: 60,
+              padding: 5,
+              height: 100
+            }}
+            circleStyle={{ width: 90, height: 90, borderRadius: 45 }}
+            duration={300}
+          />
+        </View>
         <View
           style={{
-            flexDirection: "row",
-            width: "100%",
-            justifyContent: "space-between",
-            alignItems: "center"
-          }}
-        >
-          <Icon
-            name="home-city"
-            type="material-community"
-            size={32}
-            color="gray"
-          />
-          <TouchableOpacity
-            style={{
-              marginVertical: 10,
-              width: 200
-            }}
-            onPress={this.switchSentido}
-          >
-            {this.renderIconSentido(sentido)}
-          </TouchableOpacity>
-          <Icon name="gate" type="material-community" size={32} color="gray" />
-        </View>
-        <TouchableOpacity
-          style={{
-            margin: 10,
-            backgroundColor: zootec ? "green" : "red",
+            flex: 1,
+            alignItems: "center",
             justifyContent: "center",
-            height: 50,
-            width: 200,
-            alignItems: "center"
+            width: "100%"
           }}
-          onPress={this.switchZootec}
         >
-          <Text>Zootecnia</Text>
-        </TouchableOpacity>
+          <Text style={{ color: rodando ? "green" : "red" }} h2>
+            {this.renderTextSentido()}
+          </Text>
+          <View
+            style={{
+              width: "100%",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center"
+            }}
+          >
+            <Icon
+              name="gate"
+              type="material-community"
+              size={32}
+              color="black"
+            />
+            <SwitchToggle
+              switchOn={sentido}
+              onPress={this.switchSentido}
+              backgroundColorOn={"black"}
+              backgroundColorOff={"green"}
+              circleColorOn={"green"}
+              circleColorOff={"black"}
+              containerStyle={{
+                marginTop: 16,
+                width: 160,
+                borderRadius: 30,
+                padding: 5,
+                height: 60
+              }}
+              circleStyle={{ width: 50, height: 50, borderRadius: 25 }}
+              duration={300}
+            />
+
+            <Icon
+              name="home-city"
+              type="material-community"
+              size={32}
+              color="green"
+            />
+          </View>
+          <Text style={{ color: rodando ? "green" : "red" }} h2>
+            {this.renderTextZootec()}
+          </Text>
+          <SwitchToggle
+            switchOn={zootec}
+            onPress={this.switchZootec}
+            backgroundColorOn={"green"}
+            circleColorOn={"white"}
+            circleColorOff={"red"}
+            containerStyle={{
+              marginTop: 16,
+              width: 160,
+              borderRadius: 30,
+              padding: 5,
+              height: 60
+            }}
+            circleStyle={{ width: 50, height: 50, borderRadius: 25 }}
+            duration={300}
+          />
+        </View>
       </View>
     );
   }
