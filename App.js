2dd254291bfa72158a69ae5fbf3baa4d4e709de8
AmadorZcv <lucasamadorzcv@gmail.com>
2019-09-21 09:50:34 -0300

    Removendo navegadores não utilizados

--- App.js
+++ App.js
@@ -1,25 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, { Component } from 'react';
-import { Provider } from 'react-redux'
-import store from './app/redux/store'
-import Tabs from './app/config/routes';
-
+import React, { Component } from "react";
+import Home from "./app/screens/Home";
 
 export default class App extends Component {
   render() {
-    return (
-      <Provider store={store}>
-        <Tabs />
-        </Provider>
-    );
+    return <Home />;
   }
 }
-
-
